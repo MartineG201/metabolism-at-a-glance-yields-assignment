d474ab588c40b8dc06ec00b3746e0b87a596c595
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2020-09-06 02:17:31 +0200

    Update and rename assignment-1.py to assignment.py

---
+++ assignment.py
@@ -0,0 +1,20 @@
+# Black box model - 1. Calculate the specific yield coefficients (C-mol) for all products.
+
+# Add your calculations here ...
+
+# Assign your solutions to the following variables (replace ... with your solutions)
+# Y_xs = ...
+# Y_xe = ...
+# Y_xg = ...
+
+def test_Y_xs():
+    assert Y_xs == 6.09 # C-mol
+
+def test_Y_xe():
+    assert Y_xe == 3.9 # C-mol
+
+def test_Y_xg():
+    assert Y_ge == 0.45 # C-mol
+
+    
+    
