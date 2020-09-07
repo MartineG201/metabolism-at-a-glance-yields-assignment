d7f3ad42285a24c3b019e125c78d08c2fbb0907c
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2020-09-07 23:59:05 +0200

    Assignment ready

--- assignment.py
+++ assignment.py
@@ -7,29 +7,29 @@ _ = 123456789  # just  wrong number, please ignore
 # Add your calculations here ...
 
 # Assign your solutions to the following variables (replace _ with your solutions)
-Y_xs = _
-Y_xe = _
-Y_xg = _
+Y_sx = _
+Y_se = _
+Y_sg = _
 
-def test_Y_xs():
-    assert Y_xs == pytest.approx(6.09) # C-mol
+def test_Y_sx():
+    assert Y_sx == pytest.approx(0.166, .01)
 
-def test_Y_xe():
-    assert Y_xe == pytest.approx(3.9) # C-mol
+def test_Y_se():
+    assert Y_se == pytest.approx(0.652, .01)
 
-def test_Y_xg():
-    assert Y_ge == pytest.approx(0.45) # C-mol
+def test_Y_sg():
+    assert Y_sg == pytest.approx(0.075, .01)
   
 
-# 2. Calculate the carbon balance. Does it close?
+# 2. Calculate the carbon balance.
 
 # Add your calculations here ...
 
 # Assign your solution to the following variable (replace _ with your solution)
-carbon_balance = _
+carbon_balance = _ # c-mol-? / c-mol-Glc
 
 def test_carbon_balance():
-    assert carbon_balance == pytest.approx(-0.74) # C-mol
+    assert carbon_balance == pytest.approx(0.106, 0.01)
     
 
 # 3. Assuming that CO2 is the only missing product, calculate how much CO2 was produced in the fermentation.
@@ -37,8 +37,8 @@ def test_carbon_balance():
 # Add your calculations here ...
 
 # Assign your solution to the following variable (replace _ with your solution)
-co2_produced = _
+co2_produced = _ # g/L
 
 def test_co2_produced():
-    assert co2_produced == pytest.approx(3.58) # g-CO2/l
+    assert co2_produced == pytest.approx(3.129, 0.01)
     
