f1ae51880a38d2a96e0e878d8eeb45b07f6bdc34
Nikolaus Sonnenschein <niko.sonnenschein@gmail.com>
2020-09-07 22:20:17 +0200

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,3 +1,5 @@
+import pytest
+
 # Black box model
 # 1. Calculate the specific yield coefficients (C-mol) for all products.
 
@@ -9,28 +11,33 @@ Y_xe = _
 Y_xg = _
 
 def test_Y_xs():
-    assert Y_xs == 6.09 # C-mol
+    assert Y_xs == pytest.approx(6.09) # C-mol
 
 def test_Y_xe():
-    assert Y_xe == 3.9 # C-mol
+    assert Y_xe == pytest.approx(3.9) # C-mol
 
 def test_Y_xg():
-    assert Y_ge == 0.45 # C-mol
+    assert Y_ge == pytest.approx(0.45) # C-mol
+  
 
-    
 # 2. Calculate the carbon balance. Does it close?
 
-# Assign your solutions to the following variable (replace ... with your solutions)
+# Add your calculations here ...
+
+# Assign your solution to the following variable (replace _ with your solution)
 carbon_balance = _
 
 def test_carbon_balance():
-    assert carbon_balance == -0.74 # C-mol
+    assert carbon_balance == pytest.approx(-0.74) # C-mol
     
+
 # 3. Assuming that CO2 is the only missing product, calculate how much CO2 was produced in the fermentation.
 
-# Assign your solutions to the following variable (replace ... with your solutions)
+# Add your calculations here ...
+
+# Assign your solution to the following variable (replace _ with your solution)
 co2_produced = _
 
 def test_co2_produced():
-    assert co2_produced == -0.74 # C-mol
+    assert co2_produced == pytest.approx(3.58) # g-CO2/l
     
