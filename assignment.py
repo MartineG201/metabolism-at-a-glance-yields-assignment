5ad1ebafefe6725c5b728b8ad6ce579f94a93b81
MartineG201 <89847052+MartineG201@users.noreply.github.com>
2021-09-19 20:39:37 +0200

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -4,12 +4,33 @@ _ = 123456789  # just  wrong number, please ignore
 # Black box model
 # 1. Calculate the specific yield coefficients (C-mol) for all products.
 
-# Add your calculations here ...
+#Start
+m_yeastDW1=0.02 #gDW/L
+m_glucose=10    #g/L
+
+#end
+m_EtOH = 10
+m_yeastDW2 = 2.74
+m_glycerol = 1.54
+
+#########
+
+#rates
+r_glucose= - m_glucose 
+r_yeastDW= m_yeastDW2 - m_yeastDW1
+r_EtOH= m_EtOH
+r_glycerol= m_glycerol
+
+#C-moles
+C_molGlys = 30.7
+C_molEtOH= 23
+C_molGucose= 30
+C_molYeast = X
 
 # Assign your solutions to the following variables (replace _ with your solutions)
-Y_sx = _
-Y_se = _
-Y_sg = _
+Y_sx = abs(r_yeastDW/r_glucose)* (X/C_molGucose
+Y_se = abs(r_EtOH/r_glucose)* (C_molEtOH/ C_molGucose)
+Y_sg = abs(r_glycerol/r_glucose) *(C_molGlys/ C_molGucose)
 
 def test_Y_sx():
     assert Y_sx == pytest.approx(0.166, .01)
